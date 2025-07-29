a3cccf76bfa3cbf81c4efcca0dacdf1460e3e7ff
Jamin Workman <jaminworkman@gmail.com>
2025-07-29 12:31:03 -0400

    p12 commmit 3

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 
 const userRouter = require("./users");
 const clothingRouter = require("./clothingItems");
-
-router.use("/users", userRouter);
-router.use("/items", clothingRouter);
 router.use((req, res, next) => {
   req.user = {
     _id: "6885549de4c5564ab654e15b",
   };
   next();
 });
+
+router.use("/users", userRouter);
+router.use("/items", clothingRouter);
 
 module.exports = router;
